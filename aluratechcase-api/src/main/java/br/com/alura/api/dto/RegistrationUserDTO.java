576c15a0662930f795824a4c66e28bec685e0351
Renato28 <renatonobrega87@gmail.com>
2024-04-19 15:01:01 -0300

    creating user class and record to register a user and enum for role and implementing the UserDetails class

---
+++ aluratechcase-api/src/main/java/br/com/alura/api/dto/RegistrationUserDTO.java
@@ -0,0 +1,16 @@
+package br.com.alura.api.dto;
+
+import br.com.alura.api.model.Role;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+public record RegistrationUserDTO(
+        @NotBlank String name,
+        @NotBlank String username,
+        @NotBlank String email,
+        @NotBlank String password,
+        @NotBlank Role role,
+        @NotBlank LocalDate dateCreation
+        ) {
+}
---
+++ aluratechcase-api/src/main/java/br/com/alura/api/model/Role.java
@@ -0,0 +1,8 @@
+package br.com.alura.api.model;
+
+public enum Role {
+
+    STUDENT,
+    INSTRUCTOR,
+    ADMIN;
+}
