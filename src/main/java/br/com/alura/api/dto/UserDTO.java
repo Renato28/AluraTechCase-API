fd717d904bc2a6225b8382727448086d459b702a
Renato28 <renatonobrega87@gmail.com>
2024-04-20 19:23:41 -0300

    changing the variable type from Enum to String

--- src/main/java/br/com/alura/api/dto/UserDTO.java
+++ src/main/java/br/com/alura/api/dto/UserDTO.java
@@ -3,7 +3,7 @@ package br.com.alura.api.dto;
 import br.com.alura.api.model.Role;
 import br.com.alura.api.model.User;
 
-public record UserDTO(String name, String email, Role role) {
+public record UserDTO(String name, String email, String role) {
     public UserDTO(User user) {
         this(user.getName(), user.getEmail(), user.getRole());
     }
