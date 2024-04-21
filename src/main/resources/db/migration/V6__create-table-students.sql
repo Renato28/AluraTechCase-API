4c061dd9ed0796fd780eec9c5d714bd510443e2f
Renato28 <renatonobrega87@gmail.com>
2024-04-21 16:49:04 -0300

    creating instructors and students tables that extend user

---
+++ src/main/resources/db/migration/V6__create-table-students.sql
@@ -0,0 +1,7 @@
+create table students (
+    user_id bigint not null,
+    cpf varchar(15) not null unique,
+    birthDate date not null,
+    primary key(id),
+    foreign key (user_id) references users(id)
+)
