034ef4e4c85cf06b25fcf91a38f84a5142c9eb8e
Renato28 <renatonobrega87@gmail.com>
2024-04-21 20:55:10 -0300

    creating the evaluation class to evaluate courses by users

---
+++ src/main/java/br/com/alura/api/model/Evaluation.java
@@ -0,0 +1,57 @@
+package br.com.alura.api.model;
+
+import br.com.alura.api.dto.EvaluateCourseDTO;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity(name = "Evaluation")
+@Table(name = "evaluations")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Evaluation implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private double grade;
+
+    @NotBlank
+    private String comments;
+
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    @Column(name = "evaluation_date")
+    private LocalDate evaluationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "reason_grade")
+    private String reasonGrade;
+
+    public Evaluation(EvaluateCourseDTO data) {
+        this.grade = data.grade();
+        this.comments = data.comments();
+        this.evaluationDate = data.evaluationDate();
+        this.course = data.course();
+        this.user = data.user();
+        this.reasonGrade = data.reasonGrade();
+    }
+}
