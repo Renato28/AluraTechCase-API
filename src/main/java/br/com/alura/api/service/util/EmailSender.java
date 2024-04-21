cd59c3c991f1a05fdadba05a16ee77e5f786f335
Renato28 <renatonobrega87@gmail.com>
2024-04-21 17:01:05 -0300

    implementing the EmailSender class

---
+++ src/main/java/br/com/alura/api/service/util/EmailSender.java
@@ -0,0 +1,13 @@
+package br.com.alura.api.service.util;
+
+public class EmailSender {
+    public static void send(String recipientEmail, String subject, String body) {
+        System.out.println(
+                "Simulating sending email to [%s]:\n".formatted(recipientEmail)
+        );
+        System.out.println("""
+                Subject: %s
+                 Body: %s
+                  """.formatted(subject, body));
+    }
+}
