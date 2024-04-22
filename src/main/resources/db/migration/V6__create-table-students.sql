6479c7a7b47de48aa3741de9ca544851dcea2e70
Renato28 <renatonobrega87@gmail.com>
2024-04-21 23:13:59 -0300

    changing field name in tables

--- src/main/resources/db/migration/V6__create-table-students.sql
+++ src/main/resources/db/migration/V6__create-table-students.sql
@@ -1,7 +1,7 @@
 create table students (
     id bigint not null,
     cpf varchar(15) not null unique,
-    birthDate date not null,
+    birth_date date not null,
     primary key(id),
     foreign key (id) references users(id)
 )
