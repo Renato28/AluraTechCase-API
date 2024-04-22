7dfa5beac5f6de2ddeaa8cc868e9c78b279ec088
Renato28 <renatonobrega87@gmail.com>
2024-04-21 21:03:30 -0300

    implementing evaluation service with business rules and validations

---
+++ src/main/java/br/com/alura/api/service/EvaluationService.java
@@ -0,0 +1,29 @@
+package br.com.alura.api.service;
+
+import br.com.alura.api.dto.EvaluateCourseDTO;
+import br.com.alura.api.model.Evaluation;
+import br.com.alura.api.repository.EvaluationRepository;
+import br.com.alura.api.service.util.EmailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EvaluationService {
+
+    @Autowired
+    private EvaluationRepository evaluationRepository;
+
+    public void evaluateCourse(EvaluateCourseDTO data) {
+        evaluationRepository.save(new Evaluation(data));
+        if (data.grade() < 6) {
+            sendInstructorNotification(data);
+        }
+    }
+
+    private void sendInstructorNotification(EvaluateCourseDTO data) {
+        String subject = "Low Rating Received";
+        String body = "A student gave the course a low rating. \n" +
+                "Reason: " + data.reasonGrade();
+        EmailSender.send(data.user().getEmail(), subject, body);
+    }
+}
